4a71c1d95e43f657cbd1d4ce479f4ed6f0b8b6cc
Devin Fox <devinfoxbusiness@gmail.co>
2017-10-06 15:03:53 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+var items = [kylie, kim, kris, kanye, caitlyn_jenner, bruce_jenner, crying_kim, kylie_derp, kanye_mad]
+
+var one = Math.floor((Math.random()*9)+1)
+var two = Math.floor((Math.random()*9)+1)
+var three = Math.floor((Math.random()*9)+1)
