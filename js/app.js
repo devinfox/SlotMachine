cde710b2b52feeebe420fcd6640a6742016400e2
Devin Fox <devinfoxbusiness@gmail.co>
2017-10-13 12:03:48 -0700

    final commit

--- js/app.js
+++ js/app.js
@@ -1,42 +1,47 @@
-
 //set the items for each roll on the slot machine
-var weights = [0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5]
-var items = [
-    {
-        name: "Kanye Mad Bitch!",
+var weights = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5]
+var items = [{
+        name: "Kanye Mad!",
         url: "url('https://i.imgur.com/ADklBcq.jpg')",
-        payout: -100
+        payout: -100,
+        audio: "https://s1.vocaroo.com/media/download_temp/Vocaroo_s1Ani73F0xb4.mp3"
     },
     {
         name: "kylie_derp",
-        url: "url('https://i.imgur.com/WECmV1U.jpg')",
-        payout: 100
+        url: "url('https://imgur.com/QtfNDAx.jpg')",
+        payout: 100,
+        audio: "https://s1.vocaroo.com/media/download_temp/Vocaroo_s1Ani73F0xb4.mp3"
     },
     {
         name: "crying_kim",
-        url: "url('https://i.imgur.com/QtfNDAx.jpg')",
-        payout: 500
+        url: "url('https://i.imgur.com/0t56DZH.jpg')",
+        payout: 500,
+        audio: "https://s1.vocaroo.com/media/download_temp/Vocaroo_s15JPTkCYGx1.mp3"
     },
+
     {
         name: "kanye",
         url: "url('https://i.imgur.com/nMDt2QN.jpg')",
-        payout: 1000
+        payout: 1000,
+        audio: "https://s1.vocaroo.com/media/download_temp/Vocaroo_s1iDlf7DSP6i.mp3"
     },
     {
         name: "kim",
         url: "url('https://i.imgur.com/vH3pQk1.jpg')",
-        payout: 5000
+        payout: 5000,
+        audio: "http://freesound.org/data/previews/361/361346_5506271-lq.mp3"
     },
     {
         name: "kylie",
-        url: "url('https://i.imgur.com/L5MSS1c.jpg')",
-        payout: 10000
+        url: "url('https://i.imgur.com/hK9e5cU.jpg')",
+        payout: 10000,
+        audio: "http://freesound.org/data/previews/69/69690_866625-lq.mp3"
     }
 ];
 
 
 
-    
+
 // cached elements
 var reel1 = $('#reel-1');
 var reel2 = $('#reel-2');
@@ -47,75 +52,115 @@ var $reels = $('.reel');
 var reels;
 var winnings;
 var bankroll;
-var score = document.getElementsByClassName("score");
+var Highscores;
+var score = $('.score');
+var high_scores = $('#highscores');
+var leverSound = new Audio("http://freesound.org/data/previews/316/316931_1015240-lq.mp3");
+var player = new Audio();
 
-$(function () {
+$(function() {
 
-// event listener for slot randomizer
-$('#lever').on('click', play);
+    // event listener for slot randomizer
+    $('#lever').on('click', play);
 
-function play()  {
-    // generate reels
-    bankroll -= 10;
-    reels = reels.map(function() {
-        return getRandomSymbol();
-    });
-    
-    winnings = computeWinnings();
-    bankroll += winnings;
-    render();
-};
+    function play() {
+        leverSound.play();
+        bankroll -= 10;
 
-function getRandomSymbol() {
-    return weights[Math.floor((Math.random() * weights.length))];
-}
+        reels = reels.map(function() {
+            return getRandomSymbol();
+        });
 
-function init() {
-    reels = [null, null, null];
-    bankroll = 1000;
-};
+        winnings = computeWinnings();
+        bankroll += winnings;
+        checkGameOver();
 
-function computeWinnings() {
-    if (reels[0] === reels[1] && reels[0] === reels[2]) {
-        return items[reels[0]].payout;
-        console.log(items[reels[0]].payout);
-    } else {
-        return 0;
+        render();
+    };
+
+    function getRandomSymbol() {
+        return weights[Math.floor((Math.random() * weights.length))];
     }
-    
-};
 
-// function scoreBoard(); {
-//     for(i=0, i>=index, i++)
-// }
-// function checkforMatch() {
-    // if (state[one] === state[one] = state[one]) = 1;
-    // win[two] = win[two] = win[two] = 2;
-//    if (reel1image === reel2image && reel1image === reel3image) {
-//        if (reel1image = one)
+    function init() {
+        reels = [null, null, null];
+        bankroll = 1000;
+    };
 
-    // win[four] = win[four] = win[four] = 4;
-    // win[five] = win[five] = win[five] = 5;
-    // win[four] = win[four] = win[four] = 6;
-    
-// }
+    function computeWinnings() {
+        if (reels[0] === reels[1] && reels[0] === reels[2]) {
+            player.src = items[reels[0]].audio;
+            player.play();
+            return items[reels[0]].payout;
+            // console.log(items[reels[0]].payout);
+        } else {
+            return 0;
+        }
+
+    };
+
+    function determineHighBankRoll() {
+        console.log(localStorage.getItem("Highscore"))
+        if (localStorage.getItem("Highscore")) {
+            var currentHighScore = parseInt(localStorage.getItem("Highscore"))
+            if (currentHighScore < bankroll) {
+                localStorage.setItem("Highscore", bankroll.toString())
+            }
+        } else {
+            localStorage.setItem("Highscore", "1000");
+        }
+    }
+
+    function render() {
+        // render the reels
+        // pick three random urls first
+        renderRandom();
+
+        setTimeout(function() {
+            renderRandom();
+        }, 200)
+        setTimeout(function() {
+            // pick the real one
+            renderReal();
+        }, 400)
+
+        //push score to score board
+        $('.score').text('$' + bankroll);
+        determineHighBankRoll();
+        $('#highscores').html(window.localStorage.getItem("Highscore"));
+    };
 
 
-function render() {
-    // render the reels
-    $reels.each(function(idx) {
-        $(this).css('background-image', reels[idx] !== null ? items[reels[idx]].url : '');
+
+    function renderRandom() {
+        $reels.each(function(idx) {
+            $(this).css('background-image', reels[idx] !== null ? items[Math.floor(Math.random() * 6)].url : '');
+        });
+    }
+
+    function renderReal() {
+        $reels.each(function(idx) {
+            $(this).css('background-image', reels[idx] !== null ? items[reels[idx]].url : '');
+        });
+    }
+
+    function checkGameOver() {
+        if (bankroll < 0) {
+            alert("Sorry, You Lose!");
+            init();
+        }
+    }
+
+    $('#init').on('click', function() {
+        bankroll = 1000;
+        $('.score').text(bankroll);
+        reels = [null, null, null];
+        render();
     });
-    $('.score').text(bankroll);
-}
 
-$('#init').on('click', function() {
-    bankroll = 1000;
-    $('.score').text(bankroll);
-    reels = [null, null, null];
+
+
+
+    init();
     render();
-});
-
-init();
-render();
 });
---
+++ readme.md
@@ -0,0 +1,17 @@
+##KARDASHIAN SLOT MACHINE
+The concept of the Kardashian themed Slot Machine is similar to any basic slot machine you'd find in Vegas, or any casino. Get three matches in a row, and you gain - or lose a certain amount of money depending on the value of those cards. Although, each time you pull the lever, you lose $10 if the spin was no match.
+
+##How it was developed
+The slot machine game was developed using HTML, CSS, and Javascript with jQuery shorthands. 
+There is an event listener on click when the lever is pressed, that uses math.random to sort through an array of numbers, that correlates to the object below it that has all the characters & their picture. Each character has a certain number of points (or payout) who's is then pushed to the score div I created in HTML. If the player reaches 0, the game is over and the highest score they ever reached is saved in the upper left corner.
+
+#How to play
+This game is very simple to understand. Pull the lever to the right of the machine and watch as the reels randomize the results. If you get three mad Kanyes, lose 100 dollars, if you get three derp Kylies, gain 100 dollars, if you get three crying Kims, gain 500 dollars, three happy Kanyes get you 1000 dollars, three Kims get you 5000 dollars and three Kylies get you 10,0000 dollars. Be careful though, if you let your scoreboard get to 0 dollars, you lose and you have to start all over. 
+
+![image of init screen](https://i.imgur.com/osvulWG.png)
+
+##Installation
+No installation required, just press the link to the game is [here](https://devinfox.github.io/SlotMachine/)
+
+to learn more about my process you can visit my Trello
+[here](https://trello.com/b/sfbrtt0J/slot-machine)
